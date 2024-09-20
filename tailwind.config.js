0db581cb68d83388b4c863d63b1c0e8f2ccd4c86
TokolohoMohale <150892343+TokolohoMohale@users.noreply.github.com>
2024-09-20 21:28:47 +0200

    feat: added configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      // sm: '480px',
+      // md: '768px',
+      // lg: '976px',
+      // xl: '1440px',
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
     extend: {
       colors: {
-        primary: "0B5ED7",
+        primary: "#0B5ED7",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
